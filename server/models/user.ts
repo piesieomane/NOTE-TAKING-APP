1753a6bf6e31bc0f94bde624882524d82ee96105
piesieomane <piesieyeboah@gmail.com>
2022-12-19 18:41:29 +0000

    create model for user

---
+++ server/models/user.ts
@@ -0,0 +1,59 @@
+'use strict';
+import { Model } from 'sequelize';
+
+interface UserAttributes {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class User extends Model<UserAttributes> implements UserAttributes {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    public id!: string;
+    public name!: string;
+    public email!: string;
+    public password!: string;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+    static associate(models: any) {
+      // define association here
+      User.belongsToMany(models.Note, {
+        through: 'UserNote',
+      });
+    }
+  }
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+  return User;
+};
