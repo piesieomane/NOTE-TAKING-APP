69cddad6dcb80dbe72c9784b8072f09b1e5dcd8c
piesieomane <piesieyeboah@gmail.com>
2022-12-21 17:36:52 +0000

    Add method for fetching users from its endpoint

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 
 import './WelcomeStyles.css';
 
 const Welcome = () => {
-  const notes = useSelector((state) => state.notes);
+  const dispatch = useDispatch();
 
-  const handleClick = () => {
-    console.log(notes);
+  const [formStates, setFormStates] = useState({ name: '' });
+
+  const changeState = (e) => {
+    setFormStates({ ...formStates, [e.target.name]: e.target.value });
+  };
+
+  const userState = (e) => {
+    e.preventDefault();
+    const userFetched = {
+      name: formStates.name,
+    };
+    dispatch(addUser(userFetched));
+    setFormStates({ name: '' });
   };
   return (
     <>
@@ -18,6 +29,17 @@ const Welcome = () => {
           Note is a simple note taking app that allows you to create, edit, and
           delete notes.
         </p>
+        <p>As a new USER you need to add yourself</p>
+        <form onSubmit={userState}>
+          <input
+            type="text"
+            name="name"
+            onChange={changeState}
+            value={formStates.name}
+            placeholder="Enter your name"
+          />
+          <button type="submit">Add</button>
+        </form>
       </div>
     </>
   );
---
+++ client/src/redux/users/users.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const ADD_USER = 'users/ADD_USER';
+const DELETE_USER = 'users/DELETE_USER';
+const UPDATE_USER = 'users/UPDATE_USER';
+const GET_USERS = 'users/GET_USERS';
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'users/ADD_USER/fulfilled':
+      return [...state, action.payload.users];
+    case 'users/DELETE_USER/fulfilled':
+      return state.filter((user) => user.id !== action.payload.id);
+    case 'users/UPDATE_USER/fulfilled':
+      return state.map((user) =>
+        user.id === action.payload.id ? action.payload : user
+      );
+    case 'users/GET_USERS/fulfilled':
+      return action.payload.users;
+    default:
+      return state;
+  }
+};
+
+export const fetchUSERS = createAsyncThunk(GET_USERS, async () => {
+  const res = await axios.get('http://localhost:3000/api/users/');
+  return {
+    users: res.data,
+  };
+});
