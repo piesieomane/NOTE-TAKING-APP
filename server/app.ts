ad91b3f9d141bd1c4c6af322a13a047943071d86
piesieomane <piesieyeboah@gmail.com>
2022-12-21 17:51:58 +0000

    Allow cors for http requests

--- server/app.ts
+++ server/app.ts
@@ -24,6 +24,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/notes', noteRouter);
 app.use('/api/users', userRouter);
 
+//allow CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
