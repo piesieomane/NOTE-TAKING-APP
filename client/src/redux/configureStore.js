5c532cfee3bc693d4ef824caed24c5fc2b214a96
piesieomane <piesieyeboah@gmail.com>
2022-12-21 17:42:30 +0000

    add user reducers to the store

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -5,9 +5,11 @@ import {
 } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import notesReducer from './notes/notes';
+import usersReducer from './users/users';
 
 const rootReducer = combineReducers({
   notes: notesReducer,
+  users: usersReducer,
 });
 
 const store = configureStore(
