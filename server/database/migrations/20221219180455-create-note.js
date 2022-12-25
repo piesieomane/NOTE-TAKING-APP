ccbf4644a5b6d37c259498877f7a0952f6887d63
piesieomane <piesieyeboah@gmail.com>
2022-12-25 17:36:11 +0000

    fix linter errors in migration files

--- server/database/migrations/20221219180455-create-note.js
+++ server/database/migrations/20221219180455-create-note.js
@@ -1,33 +1,31 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Notes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      content: {
-        type: Sequelize.TEXT,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Notes');
-  },
-};
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('Notes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    content: {
+      type: Sequelize.TEXT,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('Notes');
+}
--- server/database/migrations/20221219192950-create-user-note.js
+++ server/database/migrations/20221219192950-create-user-note.js
@@ -1,45 +1,43 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UserNotes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: {
-            tableName: 'Users',
-          },
-          key: 'id',
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('UserNotes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: 'Users',
         },
+        key: 'id',
       },
-      noteId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: {
-            tableName: 'Notes',
-          },
-          key: 'id',
+    },
+    noteId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: 'Notes',
         },
+        key: 'id',
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UserNotes');
-  },
-};
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('UserNotes');
+}
