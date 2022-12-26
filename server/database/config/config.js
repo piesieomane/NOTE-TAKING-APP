976150252f2d0d7e1fd269c0144f7ba0d2c894f3
piesieomane <piesieyeboah@gmail.com>
2022-12-26 16:16:27 +0000

    fix issue while deploying

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 
-export const development = {
+const development = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   host: '127.0.0.1',
   dialect: 'postgres',
 };
-export const test = {
+const test = {
   username: 'root',
   password: null,
   database: 'database_test',
   host: '127.0.0.1',
   dialect: 'mysql',
 };
-export const production = {
+const production = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
@@ -22,3 +22,5 @@ export const production = {
   host: 'containers-us-west-82.railway.app',
   dialect: 'postgres',
 };
+
+module.exports = { development, test, production };
